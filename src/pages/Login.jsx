278e0c8a6db55709793c0b52cc5fa92d1ef025c9
Ayaan Malik <ayaan.malik.in@gmail.com>
2023-12-23 14:17:35 +0530

    login for making commit 2

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import loginImage from "../components/images/loginBg.jpg";
 
 const Login = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+
   const backgroundImageStyle = {
     backgroundImage: `url(${loginImage})`,
-    backgroundRepeat: 'no-repeat',
+    backgroundRepeat: 'no-repeat',  
     backgroundSize: 'cover',
     backgroundPosition: 'center',
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Validate form data
+    if (!formData.username || !formData.password) {
+      alert('Please fill in required fields');
+      return;
+    }
+
+    // Continue with form submission or other logic
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData(prevFormData => ({
+      ...prevFormData,
+      [name]: value,
+    }));
   };
 
   return (
@@ -15,31 +40,35 @@ const Login = () => {
       className="loginBg d-flex align-items-center justify-content-center text-center"
       style={{ ...backgroundImageStyle, minHeight: "100vh" }}
     >
-      <form className="p-5 rounded bg-light shadow-lg">
+      <form className="p-5 rounded bg-light shadow-lg login-form" onSubmit={handleSubmit}>
         <h1 className="my-3">Login</h1>
 
         <div className="my-2">
           <input
-            type="email"
+            type="text"
+            name="username"
             className="form-control py-2"
-            placeholder="Username or Email"
-          />
-        </div>
-        <div className="my-2">
-          <input
-            type="password"
-            className="form-control py-2"
-            placeholder="Password"
+            placeholder="Username"
+            value={formData.username}
+            onChange={handleChange}
           />
         </div>
 
-        <div className="my-2 text-start">
-          <input type="checkbox" id="rememberMe" />
-          <label htmlFor="rememberMe" className="ms-2">Remember me</label>
+        <div className="my-2">
+          <input
+            type="password"
+            name="password"
+            className="form-control py-2"
+            placeholder="Password"
+            value={formData.password}
+            onChange={handleChange}
+          />
         </div>
+
         <button className="w-100 btn btn-primary py-2" type="submit">
           Sign in
         </button>
+
         <p className="my-2">
           If you don't have an account, please <Link to="/signup">Sign up</Link> here
         </p>
