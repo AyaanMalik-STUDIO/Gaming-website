b491c4a20bddb3b6feacaecd9bc7a8747326322d
Ayaan Malik <ayaan.malik.in@gmail.com>
2023-12-28 15:05:55 +0530

    leftside bar commit V1

--- src/components/LeftSidebar.jsx
+++ src/components/LeftSidebar.jsx
@@ -1,53 +1,79 @@
 // LeftSidebar.js
-import React from 'react';
+import React from "react";
+import Img from "../images/11.jpg";
+import Img1 from "../images/12.jpg";
+
 
 const LeftSidebar = () => {
-  // Example data for recent posts
-  const recentPosts = [
-    {
-      id: 1,
-      title: "Retro Coding: A Journey Back in Time",
-      content: "Take a nostalgic trip back to the roots of coding. Explore the vintage programming languages and techniques that shaped the early days of computing.",
-      imageSrc: "https://placekitten.com/200/201", // Replace with your actual image URL
-      link: "/retro-coding"
-    },
-    {
-      id: 2,
-      title: "The Art of Pixelated Graphics in Retro Games",
-      content: "Dive into the world of pixel art and discover how early video game designers created stunning graphics with limited resources. Learn the secrets behind the charm of retro games.",
-      imageSrc: "https://placekitten.com/200/202", // Replace with your actual image URL
-      link: "/pixelated-graphics"
-    },
-    {
-      id: 3,
-      title: "Creating Retro-Style Websites with Modern Tools",
-      content: "Combine the best of both worlds: modern web development tools and a retro design aesthetic. Learn tips and tricks for building websites that evoke the nostalgic feel of the past.",
-      imageSrc: "https://placekitten.com/200/203", // Replace with your actual image URL
-      link: "/retro-websites"
-    },
-    // Add more posts as needed
-  ];
-
   return (
-    <aside className="left-sidebar">
+    <aside className="left-sidebar py-2">
       <div className="sidebar-item">
-        <h2>Recent Posts</h2>
-        {recentPosts.map(post => (
-          <div key={post.id} className="card mb-3">
-            <div className="row g-0">
-              <div className="col-md-4">
-                <img src={post.imageSrc} className="img-fluid rounded-start" alt={`Post ${post.id}`} />
-              </div>
-              <div className="col-md-8">
-                <div className="card-body">
-                  <h3 className="card-title">{post.title}</h3>
-                  <p className="card-text">{post.content}</p>
-                  <a href={post.link} className="btn btn-primary">Read More</a>
-                </div>
+        {/* First Card */}
+        <div className="card mb-3">
+          <div className="row g-0">
+            {/* First Column (Image) */}
+            <div className="col-md-6">
+              <img
+                src={Img}
+                className="img-fluid"
+                alt="..."
+                style={{ height: 200 }}
+              />
+            </div>
+            {/* Second Column (Card Body) */}
+            <div className="col-md-6">
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">
+                  This is a wider card with supporting text below as a natural
+                  lead-in to additional content.
+                </p>
+                <p className="card-text">
+                  <small className="text-body-secondary">
+                    Last updated 3 mins ago
+                  </small>
+                </p>
               </div>
             </div>
           </div>
-        ))}
+        </div>
+
+        <hr />
+
+        {/* Second Card */}
+        <div className="card mb-3">
+          <div className="row g-0 d-flex justify-content-between">
+            {/* First Column (Card Body) */}
+            <div className="col-md-6">
+              <div className="card-body">
+                <h2 className="card-title">Card title</h2>
+                <p>
+                  <small className="text-body-secondary">
+                    Last updated 3 mins ago
+                  </small>
+                </p>
+              </div>
+            </div>
+            {/* Second Column (Image) */}
+            <div className="col-md-6 text-end">
+              <img
+                src={Img1}
+                className="img-fluid"
+                alt="..."
+                style={{ height: 150 }}
+              />
+            </div>
+          </div>
+          {/* Content Below Image */}
+          <div className="row g-0 mt-2">
+            <div className="col-md-12">
+              <p className="card-text">
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </aside>
   );
