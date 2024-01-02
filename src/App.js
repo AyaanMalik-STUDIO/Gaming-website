9588cd5a5605d784713a22d1fd61e90d3954b530
Ayaan Malik <ayaan.malik.in@gmail.com>
2024-01-02 20:09:34 +0530

    build deploy V2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // App.js
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import './App.css';
